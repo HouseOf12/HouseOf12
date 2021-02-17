b068f7a5bec34ed33592411c7ca3700b66ab0b56
Gabbytam <gtam2@fordham.edu>
2021-02-16 17:27:57 -1000

    adding more params to create user and translating location to lat and lon

--- pages/api/users.js
+++ pages/api/users.js
@@ -11,7 +11,7 @@ const handler = nc();
 handler.use(all);
 
 handler.post(async (req, res) => {
-  const { name, password } = req.body;
+  const { name, password, day, month, year, hour, minute, lat, lon  } = req.body;
   const email = normalizeEmail(req.body.email);
   if (!isEmail(email)) {
     res.status(400).send('The email you entered is invalid.');
@@ -27,7 +27,7 @@ handler.post(async (req, res) => {
   }
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = await insertUser(req.db, {
-    email, password: hashedPassword, bio: '', name,
+    email, password: hashedPassword, bio: '', name, day, month, year, hour, minute, lat, lon
   });
   req.logIn(user, (err) => {
     if (err) throw err;
