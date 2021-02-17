d08bbac422f6246f29fafaaaf8b04edc7a56214b
Gabbytam <gtam2@fordham.edu>
2021-02-16 15:21:57 -1000

    able to grab and pass down the current user to have access to its info

--- pages/natalchart.js
+++ pages/natalchart.js
@@ -1,15 +1,33 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
 import Natalchart from '../components/Natalchart.js'
-export const natalchart = () => {
+
+//imports for getServerSideProps function 
+import { all } from '../middlewares/index';
+import { extractUser } from '../lib/api-helpers';
+import { findUserById } from '../db/index';
+
+//export const natalchart = ({user}) => {
+export default function natalchart({ user }) {
+    console.log('the user', user)
     return (
         
         <Box>
             <Box zIndex="-1"  bgPosition="center" bgSize="cover" minH="100%" minW="32vw" w="100vw" h="auto" position="fixed" overflowX="scroll" top="0" right="0" bgImage="url('./chartwpp.jpg')" /> 
-            <Natalchart /> 
+            {/* pass down the user from props to the natalchart so it has access to specific info */}
+            <Natalchart user= {user}/>
+            
         </Box>
         
     )
 }
 
-export default natalchart
+// export default natalchart
+
+//get the current user and pass it into page component
+export async function getServerSideProps(context) {
+    await all.run(context.req, context.res);
+    const user = extractUser(await findUserById(context.req.db, context.req.user._id));
+    if (!user) context.res.statusCode = 404;
+    return { props: { user } };
+  }
